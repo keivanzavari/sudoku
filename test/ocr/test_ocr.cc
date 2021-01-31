0a0c95030d0690d59df5eb89204119b4baa09add
Keivan Zavari <keivan.zavari@gmail.com>
2021-01-31 16:20:31 +0100

    Implement the ocr test

--- test/ocr/test_ocr.cc
+++ test/ocr/test_ocr.cc
@@ -2,84 +2,75 @@
 #include <gtest/gtest.h>
 #include <tesseract/baseapi.h>
 
+#include <array>
 #include <filesystem>
+#include <memory>
+#include <numeric>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/text/ocr.hpp>
+#include <string>
+#include <thread>
 
 #include "gmock/gmock.h"
-#include "opencv2/core/mat.hpp"
-#include "opencv2/opencv.hpp"
-
 namespace su {
 
 namespace fs = std::filesystem;
 
-static constexpr int IMAGE_WIDTH = 200;
-static constexpr int IMAGE_HEIGHT = 200;
+static constexpr int IMAGE_WIDTH = 100;
+static constexpr int IMAGE_HEIGHT = 100;
 static cv::Mat createImage(const int red, const int green, const int blue) {
-  return cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3,
-                 cv::Scalar(blue, green, red));
+  return cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(blue, green, red));
 }
 
-// cv::Mat importImage(const fs::path &image_path)
-// {
-//   if (!fs::exists(image_path))
-//   {
-//     throw std::runtime_error("File does not exist.");
-//   }
-//   cv::Mat image = cv::imread(image_path.string());
-//   return image;
-// }
-
-// void OCRTester(const char* imgname, const char* groundtruth, const char*
-// tessdatadir, const char* lang) {
-//   // log.info() << tessdatadir << " for language: " << lang << std::endl;
-//   char* outText;
-//   std::locale loc("");  // You can also use "" for the default system locale
-//   std::ifstream file(groundtruth);
-//   file.imbue(loc);  // Use it for file input
-//   std::string gtText((std::istreambuf_iterator<char>(file)),
-//   std::istreambuf_iterator<char>()); std::unique_ptr<tesseract::TessBaseAPI>
-//   api(new tesseract::TessBaseAPI()); ASSERT_FALSE(api->Init(tessdatadir,
-//   lang)) << "Could not initialize tesseract."; Pix* image = pixRead(imgname);
-//   ASSERT_TRUE(image != nullptr) << "Failed to read test image.";
-//   api->SetImage(image);
-//   outText = api->GetUTF8Text();
-//   EXPECT_EQ(gtText, outText) << "Phototest.tif OCR does not match ground
-//   truth for " <<
-//   ::testing::PrintToString(lang); api->End(); delete[] outText;
-//   pixDestroy(&image);
-// }
-// testing::Matcher(IsBetween, int number, int lo, int hi) {
-//   return lo <= number <= hi;
-// }
-
-static cv::Mat drawNumberOnImage(int number) {
+cv::Mat drawNumberOnImage(const int number, bool save = false) {
   EXPECT_TRUE(0 <= number <= 9);
-  cv::Mat green_image = createImage(87, 171, 50);
+  cv::Mat image = createImage(87, 161, 50);
 
-  //   void cv::putText	(	InputOutputArray 	img,
-  // const String & 	text,
-  // Point 	org,
-  // int 	fontFace,
-  // double 	fontScale,
-  // Scalar 	color,
-  // int 	thickness = 1,
-  // int 	lineType = LINE_8,
-  // bool 	bottomLeftOrigin = false
-  // )
-  constexpr int middle_point = IMAGE_WIDTH / 2;
+  const std::string text = std::to_string(number);
   constexpr double font_scale = IMAGE_WIDTH / 40;
   static const cv::Scalar color(0, 0, 0);
-  cv::putText(green_image, std::to_string(number),
-              cv::Point(middle_point, middle_point), cv::FONT_HERSHEY_SIMPLEX,
-              font_scale, color);
-  // cv::Mat image = cv::imdecode(green_image, cv::IMREAD_COLOR);
-  cv::imwrite("/home/sudoku/image.png", green_image);
-  return green_image;
+  constexpr int thickness = 3;
+
+  int baseline = 0;
+  cv::Size text_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, font_scale, thickness, &baseline);
+  const int text_x = (IMAGE_WIDTH - text_size.width) / 2;
+  const int text_y = IMAGE_HEIGHT - text_size.height / 2;
+  cv::putText(image, text, cv::Point(text_x, text_y), cv::FONT_HERSHEY_SIMPLEX, font_scale, color, thickness);
+
+  if (save) {
+    cv::imwrite("/home/sudoku/image_" + text + ".png", image);
+  }
+  return image;
 }
 
 TEST(OCR, DrawnImage) {
-  auto image = drawNumberOnImage(0);
-  ASSERT_TRUE(true);
+  std::unique_ptr<tesseract::TessBaseAPI> api = std::make_unique<tesseract::TessBaseAPI>();
+  // Initialize tesseract-ocr with English, without specifying tessdata path
+  EXPECT_TRUE(api->SetVariable("classify_bln_numeric_mode", "1"));  // to set numeric-only mode.
+  api->SetVariable("tessedit_char_blacklist", "OSxyz");
+
+  if (api->Init(NULL, "eng")) {
+    fprintf(stderr, "Could not initialize tesseract.\n");
+    exit(1);
+  }
+  std::array<int, 10> numbers;
+  std::iota(numbers.begin(), numbers.end(), 0);
+
+  for (const auto number : numbers) {
+    cv::Mat image = drawNumberOnImage(number, true);
+    int bytes_per_pixel = image.channels();
+    int bytes_per_line = image.step1();
+    api->SetImage((uchar *)image.data, image.size().width, image.size().height, bytes_per_pixel, bytes_per_line);
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    auto out_text = std::string(api->GetUTF8Text());
+    out_text.erase(std::remove(out_text.begin(), out_text.end(), '\n'), out_text.end());
+
+    EXPECT_EQ(out_text, std::to_string(number));
+  }
+  api->End();
 }
 
 }  // namespace su
